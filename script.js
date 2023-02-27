c4b8d8b62172d7a9b0b0a90fa1d7f4c9783b3804
paddy meade <patrickmeade4@gmail.com>
2023-02-27 01:00:12 +0000

    added playRound function fixed getComputerChoice

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 function getComputerChoice() {
     let choice = ["Rock", "Paper", "Scissors"];
-    randNum = Math.floor((Math.random() * 3) +1 );
+    randNum = Math.floor((Math.random() * 3));
     return choice[randNum];
 };
 
+const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
 
+function playRound(playerSelection, computerSelection) {
+    let outcome = "invalid choice";
+    let p1 = playerSelection.toLowerCase();
+    let p2 = computerSelection.toLowerCase();
+
+    if (p1 === p2) {
+        outcome = "tied";
+    } else {
+        if ((p1 === "rock" && p2 === "scissors") ||
+            (p1 === "paper" && p2 === "rock") ||
+            (p1 === "scissors" && p2 === "paper")) {
+                outcome = "win"
+            } else { outcome = "lose"}}
+    
+            return `you ${outcome} you picked ${p1} the computer picked ${p2}`;
+        }
+   
+  
+  console.log(playRound(playerSelection, computerSelection));
