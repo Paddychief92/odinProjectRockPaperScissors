81324c3608921225140f0a4c085315d7110d006f
paddy meade <patrickmeade4@gmail.com>
2023-03-12 01:27:16 +0000

    added dom manipulation to all buttons

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 const round = document.getElementById("round");
 const scores = document.getElementById("scores");
+const winner = document.getElementById("finalResult");
+const buttons = document.getElementById("buttons");
 let playerScore = 0;
 let computerScore = 0;
 
@@ -11,15 +13,24 @@ rock.addEventListener("click", () => {
   round.innerText = rockResult;
   let rockScore = incScore(rockResult);
   scores.innerText = rockScore;
-
+  let gameWinner = game();
+  winner.innerText = gameWinner;
 });
 paper.addEventListener("click", () => {
   let paperResult = playRound("paper", getComputerChoice());
   round.innerText = paperResult;
+  let paperScore = incScore(paperResult);
+  scores.innerText = paperScore;
+  let gameWinner = game();
+  winner.innerText = gameWinner;
 });
 scissors.addEventListener("click", () => {
   let scissorResult = playRound("scissors", getComputerChoice());
   round.innerText = scissorResult;
+  let scissorsScore = incScore(scissorResult);
+  scores.innerText = scissorsScore;
+  let gameWinner = game();
+  winner.innerText = gameWinner;
 });
 
 function getComputerChoice() {
@@ -61,15 +72,14 @@ function playRound(playerSelection, computerSelection) {
 
    function game() {
     
-
-     
-
-      if (playerScore > computerScore) {
-        return "you win the game";
-      } else if (playerScore < computerScore) {
-        return "computer wins the game";
+      if (playerScore === 5) {
+        buttons.remove();
+        return "you win the game refresh to play again!";
+      } else if (computerScore === 5) {
+        buttons.remove();
+        return "computer wins the game refresh to play again!";
       } else {
-        return "it is a draw"};
+        return "First to 5 wins!"};
       };
 
         
